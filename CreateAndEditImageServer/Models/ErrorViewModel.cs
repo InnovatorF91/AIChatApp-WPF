ac7ada9ce6071c21a8939368c028b77e7f516f5f
InnovatorF91 <club_395488567@yahoo.co.jp>
2025-06-01 19:34:22 +0900

    刪除不要的文件
    刪除不要的文件

--- CreateAndEditImageServer/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CreateAndEditImageServer.Models
-{
-	public class ErrorViewModel
-	{
-		public string? RequestId { get; set; }
-
-		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-	}
-}
